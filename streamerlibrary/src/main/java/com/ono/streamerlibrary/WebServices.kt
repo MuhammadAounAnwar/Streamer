d6282ae83f09e21f26aedc888f9dc246febd7081
Muhammad Aoun <51170716+MuhammadAounAnwar@users.noreply.github.com>
2022-10-04 10:22:49 +0500

    Media detail UI Screen

--- streamerlibrary/src/main/java/com/ono/streamerlibrary/WebServices.kt
+++ streamerlibrary/src/main/java/com/ono/streamerlibrary/WebServices.kt
@@ -1,12 +1,11 @@
 package com.ono.streamerlibrary
 
-import retrofit2.http.*
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface WebServices {
-//    @GET("registerUser")
-//    suspend fun getRegisteredUsers(
-//        @Query("username") userName: String,
-//        @Query("externalId") externalId: String
-//    ): ApiResponse<List<RegisteredUser>>
+    @GET("registerUser")
+    suspend fun getRegisteredUsers(@Query("username") userName: String, @Query("externalId") externalId: String): Response<Any>
 
 }
