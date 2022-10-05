a0267ae3a34401aec90dbbb0ead300e9071dad23
Muhammad Aoun <51170716+MuhammadAounAnwar@users.noreply.github.com>
2022-10-05 18:59:33 +0500

    Code clean

--- app/src/main/java/com/ono/streamer/ui/helper/Util.kt
+++ app/src/main/java/com/ono/streamer/ui/helper/Util.kt
@@ -1,4 +1,17 @@
 package com.ono.streamer.ui.helper
 
+import android.util.Log
+import com.google.gson.Gson
+import retrofit2.Response
+
 object Util {
+
+    private val TAG = "Util"
+
+    private fun convertJsonResponse(response: Response<Any>, parseClass: Class<*>) {
+        val gson = Gson()
+        val msg = response.errorBody()?.string()
+        val testModel = gson.fromJson(msg, parseClass)
+        Log.e(TAG, "convertJsonResponse: $testModel")
+    }
 }
--- app/src/main/java/com/ono/streamer/ui/mainscreen/Repository.kt
+++ app/src/main/java/com/ono/streamer/ui/mainscreen/Repository.kt
@@ -2,14 +2,10 @@ package com.ono.streamer.ui.mainscreen
 
 import android.util.Log
 import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
-import com.google.gson.stream.JsonReader
 import com.ono.streamerlibrary.RetrofitHelper
 import com.ono.streamerlibrary.WebServices
 import com.ono.streamerlibrary.models.ResponseModel
 import retrofit2.Response
-import java.io.StringReader
-import java.lang.reflect.Type
 
 
 private const val TAG = "IRepository"
@@ -56,38 +52,4 @@ class ISourceImpl : ISource {
         }
         return response
     }
-
-
-    private fun convertSuccessResponse(response: Response<Any>, parseClass: Class<*>) {
-        val gson = Gson()
-        val msg = response.body().toString()
-//        Log.e(TAG, "convertSuccessResponse--1: $msg")
-        val reader = JsonReader(StringReader(msg))
-        reader.isLenient = true
-        kotlin.runCatching {
-
-            val collectionType: Type = object : TypeToken<ResponseModel?>() {}.type
-            val articles: ResponseModel = gson.fromJson(reader, collectionType)
-            Log.e(TAG, "convertSuccessResponse--2: ${gson.toJson(articles)}")
-
-//            val testModel = gson.fromJson(reader, parseClass) as ResponseModel
-
-//            val moshi = Moshi.Builder().build()
-//            val jsonAdapter: JsonAdapter<ResponseModel> = moshi.adapter(ResponseModel::class.java)
-//            val responseModel: ResponseModel = jsonAdapter.fromJson(msg)!!
-
-//            Log.e(TAG, "convertSuccessResponse--2: $responseModel")
-        }.onFailure {
-            it.printStackTrace()
-        }
-    }
-
-    private fun convertJsonResponse(response: Response<Any>, parseClass: Class<*>) {
-        val gson = Gson()
-        val msg = response.errorBody()?.string()
-        val testModel = gson.fromJson(msg, parseClass)
-        Log.e(TAG, "convertJsonResponse: $testModel")
-    }
-
-
 }
